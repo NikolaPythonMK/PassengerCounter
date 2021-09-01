8599c313c84312678af5dcd00559584b7ae9d9b1
NikolaPythonMK <76405057+NikolaPythonMK@users.noreply.github.com>
2021-09-01 18:24:00 +0200

    Delete PassengerCounter directory

--- PassengerCounter/index.js
+++
@@ -1,9 +0,0 @@
-// document.getElementById("count-el").innerText = 5
-
-let count = 0;
-let countEl = document.getElementById("count-el");
-
-function increment(){
-    count++;
-    countEl.innerText = count;
-}
