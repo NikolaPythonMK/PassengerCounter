78c78ee3080ca52832a02ef3b139a12d19e72a25
NikolaPythonMK <76405057+NikolaPythonMK@users.noreply.github.com>
2021-09-02 18:38:23 +0200

    updated

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let count = 0;
 let countEl = document.getElementById("count-el");
 let incrementButton = document.getElementById("increment-btn");
+let saveButton = document.getElementById("save-btn");
+let saveEntries = document.getElementById("save-entries");
 
-incrementButton.addEventListener("click", () => countEl.innerText = ++count);
+incrementButton.addEventListener("click", () => countEl.textContent = ++count);
+
+saveButton.addEventListener("click", function(){
+    saveEntries.textContent += count + " - ";
+    count = 0;
+    countEl.textContent = 0;
+    console.log(count);
+});
